e2dd8e0dcf63c0e0e0e0273fa5334edfe89c7fd9
Alexander Koval <kovalidis@gmail.com>
2019-08-23 18:23:12 +0300

    Update alpine to 3.10

--- fetch_binaries.sh
+++ fetch_binaries.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -ex
-ALPINE_VERSION=v3.8
+ALPINE_VERSION=v3.10
 ALPINE_MIRROR=http://dl-cdn.alpinelinux.org/alpine/
-APK_TOOLS=apk-tools-static-2.10.1-r0.apk
-BUSYBOX=busybox-static-1.28.4-r1.apk
-ALPINE_KEYS=alpine-keys-2.1-r1.apk
+APK_TOOLS=apk-tools-static-2.10.4-r2.apk
+BUSYBOX=busybox-static-1.30.1-r2.apk
+ALPINE_KEYS=alpine-keys-2.1-r2.apk
 
 ARCH=${1:-x86_64}
 
 SHA1SUMS_x86_64="\
-6a669b68be304249d5d3398b8db2cc5cc23674bf  $APK_TOOLS
-e14d923f6e24f7a47bb0d8bd1cd8d0d6868d8ae8  $BUSYBOX
-4dd03fa0dfeefdd81ac13d77e0d3ed069821de33  $ALPINE_KEYS"
+23a28a47cf4cb0ab42fde4f6e6af20a8e595488a  $APK_TOOLS
+de4c07d01f3a014b341345abaf89d5792cb32eb0  $BUSYBOX
+0004212c1be4195853b9ea964b191374dfc1c36b  $ALPINE_KEYS"
 
 SHA1SUMS_armhf="\
-b30f18f5743d13f1845577e98e629a93c49212a4  $APK_TOOLS
-26a281d4c940e00eba6c9d0772acb1fb56731c32  $BUSYBOX
-a10caf9d8162d5ca16fc77729cfebf9c79d8c87b  $ALPINE_KEYS"
+70cdc74b27393e6bf514e81ef4b596f79c0f44b8  $APK_TOOLS
+6e8b46d4671bd5c36a9ae0f04fabff5bc847229f  $BUSYBOX
+963b900f31d62814d0a30d41e3596e53766844c5  $ALPINE_KEYS"
 
 FETCH_DIR="alpine/"$ARCH
 mkdir -p "$FETCH_DIR" 2>/dev/null || true
